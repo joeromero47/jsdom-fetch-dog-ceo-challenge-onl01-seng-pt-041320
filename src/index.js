69888cee007542407e05682f0cd6fe8a29511808
joeromero47 <joeromero47@gmail.com>
2020-11-18 16:40:54 -0800

    commit tree 4b9fe187c7cbe82be684587b330fc7cfe55318cb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+document.addEventListener('DOMContentLoaded', function(){
+  loadImages();
+  loadBreedOptions();
+
+});
+
+function loadImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+  .then(res => res.json())
+  .then(results => {
+    results.message.forEach(image => addImage(image))
+  });
+}
+
+function addImage(dogPicUrl) {
+  let container = document.querySelector('#dog-image-container');
+  let newImageEl = document.createElement('img')
+  newImageEl.src = dogPicUrl;
+  container.appendChild(newImageEl);
+}
+
+
 console.log('%c HI', 'color: firebrick')
